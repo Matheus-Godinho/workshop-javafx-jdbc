b6f82cce1995f46ef3094ca3765220d4af7579ea
Matheus-Godinho <matheus.brc22@gmail.com>
2020-11-17 03:40:16 -0300

    Seller TableView

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,7 +1,13 @@
 package gui.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 public class Utils {
@@ -18,5 +24,30 @@ public class Utils {
 			return null;
 		}
 	}
+	
+	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+		tableColumn.setCellFactory(column -> new TableCell<T, Date>() {
+			private final SimpleDateFormat sdf = new SimpleDateFormat(format);
+			
+			@Override
+			protected void updateItem(Date item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? null : sdf.format(item));
+			}
+		});
+	}
+	
+	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, Integer decimalPlaces) {
+		tableColumn.setCellFactory(column -> new TableCell<T, Double>() {
+			@Override
+			protected void updateItem(Double item, boolean empty) {
+				super.updateItem(item, empty);
+				Locale.setDefault(Locale.US);
+				setText(empty ? null : String.format("%." + decimalPlaces + "f", item));
+			}
+		});
+	}
+	
+
 
 }
